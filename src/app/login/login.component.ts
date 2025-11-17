492a905687c71cb80b92aabf946e1f4b580d373c
mahesh-expeed <120458363+mahesh-expeed@users.noreply.github.com>
2025-11-17 21:08:00 +0530

    Add login page component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    // Handle login logic here
+    console.log('Login attempt:', this.username, this.password);
+  }
+}
